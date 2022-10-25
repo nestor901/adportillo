9baca79e4a914d88982617a6bef0ebad2b4c73fb
Nestor Adonai Portillo <nestor901@outlook.com>
2022-10-25 15:50:27 -0600

    configurando entorno de produccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './Components/about/about.component';
+import { ExperiencesComponent } from './Components/experiences/experiences.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ExperiencesComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
