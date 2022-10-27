8be70fe7b740799bcde4d0f5526c79dc136be44a
Nestor Adonai Portillo <nestor901@outlook.com>
2022-10-27 16:08:08 -0600

    home and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormBuilder } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,17 +9,26 @@ import { AboutComponent } from './Components/about/about.component';
 import { ExperiencesComponent } from './Components/experiences/experiences.component';
 import { PortfolioComponent } from './Components/portfolio/portfolio.component';
 
+/* componentes agregados*/
+import {MatMenuModule} from '@angular/material/menu';
+import { NavComponent } from './Components/nav/nav.component';
+import { HomeComponent } from './Components/home/home.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     ExperiencesComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
